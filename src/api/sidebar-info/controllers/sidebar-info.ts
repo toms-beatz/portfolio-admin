954f325664cbc517a1a5e944464f802cd8fb56c5
toms412 <thomas.mazeau@icloud.com>
2025-02-20 15:32:05 +0100

    Add: Stapi setup and first collections

---
+++ src/api/sidebar-info/controllers/sidebar-info.ts
@@ -0,0 +1,7 @@
+/**
+ * sidebar-info controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sidebar-info.sidebar-info');
---
+++ src/api/sidebar-info/routes/sidebar-info.ts
@@ -0,0 +1,7 @@
+/**
+ * sidebar-info router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sidebar-info.sidebar-info');
